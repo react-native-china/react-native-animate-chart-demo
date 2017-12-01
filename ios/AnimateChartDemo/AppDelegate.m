9005f7508b51c73d33d5777f09520b706e1c5692
Oboo Chin <oboochin@gmail.com>
2017-12-01 15:33:15 +0800

    Update babel preset.

--- ios/AnimateChartDemo/AppDelegate.m
+++ ios/AnimateChartDemo/AppDelegate.m
@@ -18,7 +18,9 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+//  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+   jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
+
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"AnimateChartDemo"
